f7f2fb91e745bbdf65ad188c9640a1ff07a9fa4c
Luaylin <54775933+Luaylin@users.noreply.github.com>
2022-10-22 18:47:52 -0500

    Fix error in the vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+const publicPath=process.env.VUE_APP_PUBLIC_PATH||'/';
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? publicPath
+    : publicPath
 })
